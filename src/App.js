922091068f3419d5e6cd583b82c184e481c6445a
daria888-hub <daria.lee365@gmail.com>
2021-09-07 13:05:59 +1000

    updated link

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App(){
   return(
     <div className="container">
     <h1>The Weather app</h1>
-    <footer><a href="https://github.com/Daria888-hub/weather-app"> Open Sourse</a>, 
+    <footer><a href="https://github.com/Daria888-hub/weather-project"> Open Sourse</a>, 
     by Daria Lee</footer>
     </div>
   )
