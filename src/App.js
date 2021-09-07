b59651612175c69bb04de48db63c5e38b0147ee9
daria888-hub <daria.lee365@gmail.com>
2021-09-07 13:24:45 +1000

    created the weather function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./App.css"
+import Weather from "./Weather";
 
 export default function App(){
   return(
     <div className="container">
+      <Weather/>
     <h1>The Weather app</h1>
     <footer><a href="https://github.com/Daria888-hub/weather-project"> Open Sourse</a>, 
     by Daria Lee</footer>
---
+++ src/Weather.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Weather.css"
+
+export default function Weather(){
+    return(
+        <h1>Hello</h1>
+    )
+
+}
